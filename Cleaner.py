bb47a458a6b5bb8c30dda2a7633d338c02c820ea
Elizabeth Pauley <elizpauley@gmail.com>
2024-10-09 22:16:53 -0600

    Updates to cleaner class

--- Cleaner.py
+++ Cleaner.py
@@ -6,37 +6,34 @@ class cleaner:
     def __init__(self, dataset):
         self.dataset = dataset
 
-    def cleaner(self, dataFrame, dropColumns):
-
-        columnTypes = dict(zip(self.dataset.variables['name'], self.dataset.variables['type']))
-
+    def cleaner(self, dataFrame, dropColumns, classCol):
         # ADDRESS NULL VALUES WHERE COLUMNS/ROWS NEED TO BE REMOVED
         # If true class is unknown, drop the row
-        for column in dropColumns:
-            dataFrame = dataFrame.dropna(subset=[column])
+        cleanedData = dataFrame.dropna(subset=[classCol])
         # Drop any rows where all values are null
-        dataFrame = dataFrame.dropna(how = 'all')
+        cleanedData = cleanedData.dropna(how = 'all')
         # Columns must have 70% of their values for rows to remain in dataset
-        dataFrame = dataFrame.dropna(axis=1, thresh = math.floor(0.70*dataFrame.shape[0]))
+        cleanedData = cleanedData.dropna(axis=1, thresh = math.floor(0.70*cleanedData.shape[0]))
 
-        # Remove class/id columns
-        dataFrame = dataFrame.drop(columns=dropColumns, axis = 1)
+        # Remove unnecessary columns (i.e., ID columns)
+        if len(dropColumns) > 0:
+            cleanedData = cleanedData.drop(columns=dropColumns, axis = 1)
 
-        # Fill remaining empty values with mean of column
-        # for column in dataFrame.columns:
-        #     dataFrame[column] = dataFrame[column].fillna(dataFrame[column].mean())
+        # Get list of categorical column names
+        classColDataFrame = cleanedData[classCol]
+        cleanedData = cleanedData.drop(columns=classCol, axis = 1)
+        categoricalColumns = cleanedData.select_dtypes(exclude=['int', 'float']).columns.tolist()
 
         # One Hot Encoding
-        categoricalColumns = []
-        for columnName in dataFrame.columns:
-            columnRole = self.dataset.variables.loc[self.dataset.variables['name'] == columnName, 'role'].values[0]
+        cleanedData = pd.get_dummies(cleanedData, columns=categoricalColumns, dtype=int)
 
-            #ignore class column
-            if columnRole != 'Target':
-                if (columnTypes[columnName] == 'Categorical'):
-                    # add to list to one-hot encode
-                    categoricalColumns.append(columnName)
+        # Iterate through columns and fill NaN values for string/object columns
+        for col in cleanedData.select_dtypes(include='object'):
+            modeVal = cleanedData[col].mode()[0]  # Get the mode (most frequent value)
+            cleanedData[col] = cleanedData[col].fillna(modeVal)  # Fill NaN with the mode
 
-        encodedDataFrame = pd.get_dummies(dataFrame, columns=categoricalColumns)
-        
-        return encodedDataFrame
+        # Fill all other na values with the mean of each column
+        cleanedData = cleanedData.fillna(cleanedData.mean())
+        cleanedData[classCol] = classColDataFrame
+
+        return cleanedData
